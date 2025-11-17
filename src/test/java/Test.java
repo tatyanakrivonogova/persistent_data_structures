b5c8056cae09becc3901b96cf2832f69fb9bb1cd
Евгений Сыроежкин <evgeniy.resuser@icloud.com>
2025-11-17 21:09:36 +0700

    Added test

---
+++ src/test/java/Test.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculatorTest {
+
+    @Test
+    void sumWorks() {
+        int result = 2 + 2;
+        assertEquals(4, result);
+    }
+}
