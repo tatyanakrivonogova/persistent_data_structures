533642242ab96be2a7dfe1b1ac24d6fb76b6b5b5
Tatyana Krivonogova <t.krivonogova@g.nsu.ru>
2025-12-03 15:45:06 +0700

    remove magic constant 31

--- src/main/java/persistent/structures/PersistentBinaryTree.java
+++ src/main/java/persistent/structures/PersistentBinaryTree.java
@@ -182,9 +182,10 @@ public class PersistentBinaryTree<T extends Comparable<T>>
         @Override
         public int hashCode() {
             int result = value.hashCode();
-            result = 31 * result + (left != null ? left.hashCode() : 0);
-            result = 31 * result + (right != null ? right.hashCode() : 0);
-            result = 31 * result + height;
+            int hashConstant = 31;
+            result = hashConstant * result + (left != null ? left.hashCode() : 0);
+            result = hashConstant * result + (right != null ? right.hashCode() : 0);
+            result = hashConstant * result + height;
             return result;
         }
     }
